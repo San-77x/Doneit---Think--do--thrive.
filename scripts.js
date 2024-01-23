6136d984d849bb6e939532991612d5a8a00a6ac9
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-24 00:48:36 +0530

    fix - clearall button, remove button, checked button

--- scripts.js
+++ scripts.js
@@ -3,45 +3,55 @@ const listing = document.getElementById("todoList");
 const button = document.getElementById("addButton");
 const clearbtn = document.getElementById("clearAll");
 
-
 button.onclick = () => {
-    if(inputBox.value=== "")
-        alert("You Must Type a task");
-        else {
-            let li = document.createElement("li");
-            li.innerHTML = inputBox.value;
-            listing.appendChild(li);
-            let span = document.createElement("span");
-            span.innerHTML = "\u00d7";
-            li.appendChild(span);
-        }
-    inputBox.value = "";
+  if (inputBox.value === "") {
+    alert("You Must Type a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = inputBox.value;
+  const span = document.createElement("span");
+  span.textContent = "\u00d7";
+  li.appendChild(span);
+  listing.appendChild(li);
+
+  inputBox.value = "";
+  saveData();
+};
+
+listing.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
     saveData();
-    }
-    listing.addEventListener("click", function(e){
-        if(e.target.tagName === "li"){
-            e.target.classList.toggle("checked");
-        }
-        else if(e.target.tagName === "span"){
-            e.target.parentElement.remove();
-            saveData();
-        } 
-    },false);
-    function saveData(){
-        localStorage.setItem("data", listing.innerHTML);
+  }
+});
 
-    }
-    function showTask(){
-        listing.innerHTML = localStorage.getItem("data");
-    }
-    showTask();
-    
+function saveData() {
+  localStorage.setItem("data", listing.innerHTML);
+}
+function showTask() {
+  listing.innerHTML = localStorage.getItem("data");
+}
+showTask();
 
+function removeEvent(e) {
+  if (e.target.classList.contains("clearAll")) {
+    list.removeChild(e.target.parentElement);
+    list.removeChild(todoList);
+  }
+}
 
-
-
-// clearbtn.onclick = () => {
-//     if(listing.value=== "")
-//         alert("You Must Add a task first");
-//     else
-// }
+clearbtn.onclick = () => {
+  console.log("Clicked");
+  if (listing.childNodes.length === 0) {
+    alert("You Must Add a task first");
+    return;
+  }
+  localStorage.setItem("data", "");
+  listing.childNodes.forEach((e) => {
+    e.remove();
+  });
+};
