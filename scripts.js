cc2521cb54faba76d2fcba349b6d0f921fcc9cc2
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-23 23:13:30 +0530

    feat - add the clear the button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById("inputTask");
 const listing = document.getElementById("todoList");
 const button = document.getElementById("addButton");
+const clearbtn = document.getElementById("clearAll");
 
 
 button.onclick = () => {
@@ -10,6 +11,14 @@ button.onclick = () => {
             let li = document.createElement("li");
             li.innerHTML = inputBox.value;
             listing.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
         }
         return inputBox.value = ""
     }
+// clearbtn.onclick = () => {
+//     if(listing.value=== "")
+//         alert("You Must Add a task first");
+//     else
+// }
